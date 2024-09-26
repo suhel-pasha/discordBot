b818d3839affedaa8a4f60b97660a04a519a714a
suhel-pasha <suhelpashasp1998@gmail.com>
2024-09-27 01:03:19 +0530

    updates

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
